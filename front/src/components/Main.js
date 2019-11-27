68dc36317362d32aeae49d92063653cda8be089a
Fairise Kevin <kevin.fairise@student-cs.fr>
2019-11-27 09:16:14 +0100

    ajout boucle for pour afficher directement toutes les annonces sur Board

--- front/src/components/Main.js
+++ front/src/components/Main.js
@@ -4,7 +4,12 @@ import Board from "../views/Board"
 function Main(props) {
   return(
     <Fragment>
-      <Board />
+      <Board listAnnounce = {[["ST2", "26/11/19", "Maelle", "Observing Earth", "Propagation Virale", "Ca me sauverait la vie" ]
+    ,["ST2", "26/11/19", "Maelle", "Observing Earth", "Propagation Virale", "Ca me sauverait la vie" ]
+    ,["ST2", "26/11/19", "Maelle", "Observing Earth", "Propagation Virale", "Ca me sauverait la vie" ]
+    ,["ST2", "26/11/19", "Maelle", "Observing Earth", "Propagation Virale", "Ca me sauverait la vie" ]
+    ,["ST2", "26/11/19", "Maelle", "Observing Earth", "Propagation Virale", "Ca me sauverait la vie" ]
+    ,["ST2", "26/11/19", "Maelle", "Observing Earth", "Propagation Virale", "Ca me sauverait la vie" ]]} />
     </Fragment>
   )
 }
--- front/src/views/Board.js
+++ front/src/views/Board.js
@@ -5,10 +5,9 @@ function Board(props){
     return (
         <Fragment>
             <div className='card-deck'>
-                <Announce sequence='ST2' postingDate='26/11/19' name='Maelle' receivedCourse='Observing Earth' wantedCourse='Propagation virale' message='Ca me sauverait la vie !' />
-                <Announce sequence='ST2' postingDate='26/11/19' name='Maelle' receivedCourse='Observing Earth' wantedCourse='Propagation virale' message='Ca me sauverait la vie !' />
-                <Announce sequence='ST2' postingDate='26/11/19' name='Maelle' receivedCourse='Observing Earth' wantedCourse='Propagation virale' message='Ca me sauverait la vie !' />
-                <Announce sequence='ST2' postingDate='26/11/19' name='Maelle' receivedCourse='Observing Earth' wantedCourse='Propagation virale' message='Ca me sauverait la vie !' />
+                {props.listAnnounce.map(([sequence, postingDate, name, receivedCourse, wantedCourse, message])=> 
+                <Announce sequence={sequence} postingDate={postingDate} name={name} receivedCourse={receivedCourse} wantedCourse={wantedCourse} message={message} />)}
+                
             </div>
         </Fragment>
     )
