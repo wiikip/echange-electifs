19e48ddd6a8286bf58d5bc34e80141d1d4d8c6c4
Kevin Fairise <kevin.fairise@student-cs.fr>
2019-12-16 16:47:13 +0100

    ajout explications pour creer la database

--- back/routes/api.js
+++ back/routes/api.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 const fs = require('fs');
 const {Client} = require('pg');
-
+const config = {user : 'postgres', database : 'postgres', password : '123456789'}
 
 var readJson = (path) => {
     return (fs.readFileSync(require.resolve(path)))
@@ -21,7 +21,7 @@ router.post('/addToListe', function(req, res, next){
 router.get('/getListe', function(req, res){
   
     
-  const client = new Client({user : 'postgres', database : 'postgres', password : '123456789'});
+  const client = new Client(config);
   client.connect()
   client.query('SELECT * FROM Annonces', (err,rresultat,next) =>{
       
